8136fa7927add346d9d49612074fb8f92193fd00
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-23 18:07:11 +0100

    fixed: name changed to title in seeder

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +14,20 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
+        $projects = config('projects');
+
+        foreach($projects as $project){
+            $new_project = new Project();
+            $new_project->title = $project['title'];
+            $new_project->description = $project['description'];
+            $new_project->languages = $project['languages'];
+            $new_project->n_stakeholders = $project['n_stakeholders'];
+            $new_project->year = $project['year'];
+            $new_project->git_link = $project['git_link'];
+            $new_project->save();
+        }
+
+        
     }
 }
