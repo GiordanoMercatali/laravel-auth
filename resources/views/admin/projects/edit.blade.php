ff8680e41c88225b40e4eea59cb1cd35fa9192e3
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-24 19:53:53 +0100

    add: create

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -24,17 +24,17 @@
                 <input type="text" class="form-control " id="languages" name="languages" value="{{ old('languages', $project->languages) }}">
             </div>
 
-            <div class="mb-3 has-validation">
+            <div class="mb-3 ">
                 <label for="title" class="form-label">N° of Stakeholders</label>
                 <input type="text" class="form-control " id="n_stakeholders" name="n_stakeholders" value="{{ old('n_stakeholders', $project->n_stakeholders) }}">
             </div>
 
-            <div class="mb-3 has-validation">
+            <div class="mb-3 ">
                 <label for="year" class="form-label">Year</label>
                 <input type="text" class="form-control " id="year" name="year" value="{{ old('year', $project->year) }}">
             </div>
 
-            <div class="mb-3 has-validation">
+            <div class="mb-3 ">
                 <label for="git_link" class="form-label">GitHub Link</label>
                 <input type="text" class="form-control " id="git_link" name="git_link" value="{{ old('git_link', $project->git_link) }}">
             </div>
