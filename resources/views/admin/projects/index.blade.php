f22da621660b9400f7767c2783c2c2d3001eb974
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-27 13:54:29 +0100

    add: message in case you have no projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -9,41 +9,47 @@
           <a class="btn btn-primary" href="{{ route('admin.projects.create' )}}"><i class="fa-solid fa-plus"></i></a>
         </div>
 
+        @if (count($projects)>0)
         <table class="table table-striped">
-            <thead>
+          <thead>
+            <tr>
+              <th scope="col">id</th>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+              <th scope="col">Languages</th>
+              <th scope="col">N° of Stakeholders</th>
+              <th scope="col">Year</th>
+              <th scope="col">GitHub Link</th>
+              <th scope="col">More</th>
+            </tr>
+          </thead>
+          <tbody>
+              @foreach($projects as $project)
               <tr>
-                <th scope="col">id</th>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col">Languages</th>
-                <th scope="col">N° of Stakeholders</th>
-                <th scope="col">Year</th>
-                <th scope="col">GitHub Link</th>
-                <th scope="col">More</th>
+                  <th scope="row">{{ $project->id }}</th>
+                  <td>{{ $project->title }}</td>
+                  <td>{{ $project->description }}</td>
+                  <td>{{ $project->languages }}</td>
+                  <td>{{ $project->n_stakeholders }}</td>
+                  <td>{{ $project->year }}</td>
+                  <td>{{ $project->git_link }}</td>
+                  <td>
+                    <a class="btn btn-success" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}"><i class="fa-solid fa-info"></i></a>
+                    <a class="btn btn-warning" href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}"><i class="fa-solid fa-pen-to-square"></i></a>
+                    <form action="{{ route('admin.projects.destroy', ['project' => $project->slug]) }}" method="POST">
+                      @csrf
+                      @method('DELETE')
+                      <button class="btn btn-danger" type="submit"><i class="fa-solid fa-trash"></i></button>
+                    </form>
+                  </td>
               </tr>
-            </thead>
-            <tbody>
-                @foreach($projects as $project)
-                <tr>
-                    <th scope="row">{{ $project->id }}</th>
-                    <td>{{ $project->title }}</td>
-                    <td>{{ $project->description }}</td>
-                    <td>{{ $project->languages }}</td>
-                    <td>{{ $project->n_stakeholders }}</td>
-                    <td>{{ $project->year }}</td>
-                    <td>{{ $project->git_link }}</td>
-                    <td>
-                      <a class="btn btn-success" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}"><i class="fa-solid fa-info"></i></a>
-                      <a class="btn btn-warning" href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}"><i class="fa-solid fa-pen-to-square"></i></a>
-                      <form action="{{ route('admin.projects.destroy', ['project' => $project->slug]) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" type="submit"><i class="fa-solid fa-trash"></i></button>
-                      </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-          </table>
+              @endforeach
+          </tbody>
+        </table>
+        @else
+            <div class="alert alert-warning mt-4">
+              <p>No projects yet! <a href="{{ route('admin.projects.create' )}}">Create a new one.</a></p>
+            </div>
+        @endif
     </div>
 @endsection
